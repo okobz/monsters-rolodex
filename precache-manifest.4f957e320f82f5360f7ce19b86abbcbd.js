331c7e12d5863282a9ab4dcf41df221dbee6117b
okobz <okaibedi@gmail.com>
2020-06-04 13:57:17 +0100

    Updates

---
+++ precache-manifest.4f957e320f82f5360f7ce19b86abbcbd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f3b0ee33d27e942d95ef89431b53a52",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "95d4c68f5e37dc7b313a",
+    "url": "/monsters-rolodex/static/css/main.fe64c35b.chunk.css"
+  },
+  {
+    "revision": "10db5393ef03258cdec6",
+    "url": "/monsters-rolodex/static/js/2.3425a8e2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.3425a8e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95d4c68f5e37dc7b313a",
+    "url": "/monsters-rolodex/static/js/main.a948f124.chunk.js"
+  },
+  {
+    "revision": "63606864b145c5d2a7b7",
+    "url": "/monsters-rolodex/static/js/runtime-main.1485a6b1.js"
+  }
+]);
